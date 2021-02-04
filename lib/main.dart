61a15ad43c573c6da9ceae4b2728db7c99d304cc
Edilvo Lima <edilvolima@gmail.com>
2021-02-04 20:24:01 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(MaterialApp(
+    title: "Calculadora de IMC",
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _textInfo = "";
+  void _resetCampos() {
+    _formKey.currentState.reset();
+    pesoController.clear();
+    alturaController.clear();
+    setState(() {
+      _textInfo = "";
+    });
+  }
+
+  void _calcular() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text);
+      double imc = peso / (altura * altura);
+
+      if (imc < 18.6) {
+        _textInfo = "Abaixo do peso: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _textInfo = "Peso Ideal: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _textInfo = "Acima do peso: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _textInfo = "Obesidade Grau I: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _textInfo = "Obesidade Grau II: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _textInfo = "Obesidade Grau III: (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calcule seu IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetCampos)
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              widget(
+                children: <Widget>[
+                  Icon(Icons.person, size: 120, color: Colors.orange),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Sua altura (cm)",
+                      labelStyle: TextStyle(color: Colors.orangeAccent),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.orange, fontSize: 25.0),
+                    controller: alturaController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira sua altura aqui.";
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Seu peso (kg)"
+                      labelStyle: TextStyle(
+                        color: Colors.orangeAccent,
+                        fontSize: 25.0,
+                      ),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 25.0                  
+                    ),
+                    controller: pesoController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira seu peso aqui.";
+                      }
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.only(top: ))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
